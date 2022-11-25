6b5acd16e5c8f9b770ab049f9e065d220eda4f97
congqin yan <1727053412@qq.com>
2022-11-25 20:32:55 +0000

    string methods

--- src/main/java/StringStringBufferStringBuilder/StringMethods.java
+++ src/main/java/StringStringBufferStringBuilder/StringMethods.java
@@ -5,5 +5,10 @@ public class StringMethods {
         String str = new String(arr);
         return str;
     }
+    public String[] stringArray(String[] source){
+        String[] target = {"ffefef","afefefef","fefafefw"};
+        System.out.println("https://www.quora.com/How-can-I-fake-it-until-I-make-it-for-a-software-development-job");
+        return source = new String[]{"edd", "fafe", "fefe"};
+    }
 
 }
