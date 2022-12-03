cfa08390447e1576353893690628e5d702cf6316
congqin yan <1727053412@qq.com>
2022-12-03 18:26:17 +0000

    mcq in string file

--- src/main/java/StringStringBufferStringBuilder/StringMethods.java
+++ src/main/java/StringStringBufferStringBuilder/StringMethods.java
@@ -10,5 +10,32 @@ public class StringMethods {
         System.out.println("https://www.quora.com/How-can-I-fake-it-until-I-make-it-for-a-software-development-job");
         return source = new String[]{"edd", "fafe", "fefe"};
     }
+    public static void main(String[] args){
+        String a1 = "name";
+        String a2 = "name";
+        System.out.println(a1 == a2);// the a1 and a2 objects have the same address in the string constant pool
+        String a3 = new String("name");
+        System.out.println(a1 == a3);// false
+
+
+        StringMethods sm = new StringMethods();
+        System.out.println(sm instanceof StringMethods);// return true
+
+        if(true){
+            System.out.println("about");
+        }
+        // package java.long -> class number -> short/byte/long/double/float/integer
+        // a class has always a constructor (possibly automatically supplied by the java compiler)
+        // integer is the name of a class available in the package java.long
+
+        // variables are declared as private so no other class can access it, this is
+        // how data hiding is done in java using concept of encapsulation.
+        //
+        // java long is the package that is imported into every java class by default
+        int sizeof = 1;
+//        System.out.println(sizeof--);
+        System.out.println(--sizeof);
+//        System.out.println(--sizeof--); error
+    }
 
 }
