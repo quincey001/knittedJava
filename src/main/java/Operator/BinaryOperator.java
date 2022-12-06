2df33003342da34df4313236c0b2834685db39ed
congqin yan <1727053412@qq.com>
2022-12-06 12:43:02 +0000

    java mcq

---
+++ src/main/java/Operator/BinaryOperator.java
@@ -0,0 +1,8 @@
+package Operator;
+
+public class BinaryOperator {
+    /**
+     * A binary operator is an operator that operates on two operands(操作数) and manipulates them
+     * to return a result.
+     */
+}
