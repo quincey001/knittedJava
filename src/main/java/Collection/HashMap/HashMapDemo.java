226a9835b92ba5479730f2ab221d974ea74709d1
congqin yan <1727053412@qq.com>
2022-12-19 00:11:27 +0000

    traverse hashmap

---
+++ src/main/java/Collection/HashMap/HashMapDemo.java
@@ -0,0 +1,26 @@
+package Collection.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    /**
+     * Java HashMap class implements the map interface which allow us to store key and value pair
+     * where keys should be unique.
+     * if you insert the duplicate key, it will replace the element of the corresponding key.
+     */
+    public static void main(String[] args){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(1, 2);
+        map.put(7, 7);
+        map.put(5,6);
+        map.put(3, 8);
+        int maxKey = 0;
+        for(Map.Entry m : map.entrySet()){
+            if((int)m.getKey() > maxKey){
+                maxKey = (int)m.getKey();
+            }
+        }
+        System.out.println(map.get(maxKey));
+    }
+}
