fb9f64e28dd5d1f57b65fc839d4cf3ce0d0d7101
congqin yan <1727053412@qq.com>
2023-01-07 11:56:18 +0000

    advanced restful api interview question

---
+++ src/main/java/RestfulAPI/AdvancedInterviewQuestions.java
@@ -0,0 +1,55 @@
+package RestfulAPI;
+
+public class AdvancedInterviewQuestions {
+    /**
+     * 1----------------what are differences between PUT, POST AND PATCH?
+     * put used to update existing resource, replace the entire resource with a new resource
+     * post used to create resource.
+     * the patch alters only the data that is specified
+     *
+     *
+     * 2----------------what is a payload in the context of a REST api?
+     * payload refers(means) to data that has been transferred through the REST API
+     *
+     * 3-----------------what is a REST message?
+     * message refers to the back and forth communication via request and response by the client and server
+     *
+     * 4------------------what are the core components of an HTTP Request?
+     * there are five components in HTTP Request,
+     * Method,
+     * URL(used to identify the resource),
+     * http version(indicate the http version that is being used),
+     * request header(contains the metadata which can be the message format, cache settings content etc),
+     * request body(contains the content or data that is being sent ).
+     *
+     * 5-------------------what are the core components of an HTTP Response?
+     * There are four components in the HTTP Response,
+     * status code(indicates the success or failure of a request)
+     * http version
+     * response header(response metadata like content length, content type, date etc)
+     * response body(return data)
+     *
+     * 6--------------------what is an idempotent method and why are they important?
+     * when using RESTful web services, a client may make numerous requests to a server at a time,
+     * an idempotent method is one that yields the same response, regardless of(无论) how many times a
+     * request is sent.
+     * pressing a button on a webpage that makes a get request to a resource after the nth press of a
+     * button the api response will still be the same as the first press
+     *
+     * 7-------------------what is the difference between idempotent and safe http methods?
+     * idempotent method is one that can be called multiple times without changing response
+     * safe method is one that does not change a resource meaning it may read but not right
+     * for example, a get method is considered to be safe.
+     *
+     *
+     * 8-------------------explain caching in a RESTful Architecture?
+     * a client may request the same data from a rest api many times,
+     * in this case it's beneficial for for the response to be cached
+     * this way less bandwidth is needed and the client may retrieve data faster.
+     * each rest api contains specific metadata related to the caching of responses
+     * for example, the headers of cache control and expires specify what responses may
+     * be cached by who and for how long
+     *
+     *
+     */
+}
--- src/main/java/RestfulAPI/InterviewQuestions.java
+++ src/main/java/RestfulAPI/InterviewQuestions.java
@@ -2,7 +2,7 @@ package RestfulAPI;
 
 public class InterviewQuestions {
     /**
-     * how are restful apis stateless?
+     * 1----------how are restful apis stateless?
      * statefulness 有状态性，statelessness 无状态性
      * an application that is considered stateful stores data from client on its own servers.
      * if a username and password were passed from the client to the server as a form of authentication
@@ -11,24 +11,24 @@ public class InterviewQuestions {
      * On the other hand, the rest architecture requires that the client states is not stored on the server.
      *
      *
-     * Explain the HTTP methods?
+     * 2----------Explain the HTTP methods?
      * REST web service will use HTTP methods and client requests, the most common method are GET, POST,
      * PUT, DELETE. these four methods correspond to crud operations.
      *
      *
-     * Expain the HTTP State codes?
+     * 3-----------Explain the HTTP State codes?
      * restful web services uses HTTP status codes in server response, the most common types of status codes
      * are 200(successful request and response),
      * 400( client-side error),
      * 500( server-side error),
      * 404( resource not found)
      *
-     * what is a URL?
+     * 4-----------what is a URL?
      * URL stands for uniform resource identifier, it identifies every resource in the rest architecture,
      * A url can be one of two types, urn which identifies a resource through a unique and persistent name,
      * and url is your typical web address.
      *
-     * what are best practices in making the URL for RESTful web-services?
+     * 5------------what are best practices in making the URL for RESTful web-services?
      * urls should be mostly standardized when developing a restful web service,this way clients can more
      * easily work with the web service.
      * here are five best practices when making restful urls,
@@ -38,7 +38,7 @@ public class InterviewQuestions {
      * (4)use lowercase
      * (5)refrain from using file extensions
      *
-     * what are the differences between REST and SOAP?
+     * 6----------------what are the differences between REST and SOAP?
      * REST is an architecture to develop web services,
      * soap or simple object access protocol serves as a protocol for changing structured information by
      * apis.
@@ -47,7 +47,38 @@ public class InterviewQuestions {
      * Soap is used in cases where more regulated and staple data needs to be transferred.
      *
      *
-     * What are the difference between REST and AJAX?
+     * 7------------------What are the difference between REST and AJAX?
+     * Ajax refers to asynchronous JavaScript and XML, which is a collection of web technologies for
+     * making asynchronous web requests.
+     * RESTful API is an architecture for handling HTTP requests
+     * this means that a RESTful api may handle Ajax clients and that ajax may be used to send restful requests
+     * but restful api could never be implemented nor replaced by ajax.
+     *
+     * 8-----------------What are the tools that you used to develop and test REST apis?
+     * tools depend on the language used
+     * node.js frameworks like express are commonly used for testing.
+     * postman is used to ensure resources are being delivered and each facet of a rest api is performing
+     * as designed.
+     *
+     * 9------------------what are real-world examples of REST APIs?
+     * For developer, RESTful APIs are used to manipulate data using the four main HTTP Methods.
+     *
+     *
+     * 10------------------pros and cons for REST API?
+     * pros:
+     * easy to learn
+     * wide range of data transfers like json and xml
+     * statelessness
+     * scalability
+     * cons:
+     * lack of built-in security
+     * need to be versioned for backwards compatibility
+     * consistency in URLs difficult to maintain for complex projects
+     *
+     *
+     *
+     *
+     *
      *
      *
      *
