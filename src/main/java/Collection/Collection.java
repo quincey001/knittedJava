9dfb5c2d9e5dc8dd2a990bdd76eae19087d67f6e
congqin yan <1727053412@qq.com>
2023-02-08 14:04:07 +0000

    median

--- src/main/java/Collection/Collection.java
+++ src/main/java/Collection/Collection.java
@@ -1,5 +1,5 @@
 package Collection;
-
+import java.util.Arrays;
 public class Collection {
     /**
      * Collections are defined to hold all the classes and interfaces, which provide
@@ -33,3 +33,24 @@ public class Collection {
      *      * it is used to iterate the elements from a collection
      */
 }
+
+
+class Median {
+    public static double findMedian(int[] array) {
+        Arrays.sort(array);
+        int length = array.length;
+        if (length % 2 == 0) {
+            int middle1 = array[length / 2 - 1];
+            int middle2 = array[length / 2];
+            return (double) (middle1 + middle2) / 2;
+        } else {
+            return array[length / 2];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("The median of the array is: " + findMedian(array) );
+    }
+}
+
